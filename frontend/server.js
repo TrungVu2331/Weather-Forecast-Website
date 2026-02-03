630c9052322f3fee1a1a9eb81919b4476a97eced
TrungVu2331 <tvu2480@gmail.com>
2026-02-04 02:20:21 +0700

    update

--- frontend/server.js
+++ frontend/server.js
@@ -6,6 +6,10 @@ app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'main.html'));
+    res.send('Weather Forecast Backend is running');
+});
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
 });
 
-app.listen(8080, () => console.log('🔵 Server đang chạy tại http://localhost:8080'));
+app.listen(3000, () => console.log('🔵 Server đang chạy tại http://localhost:3000'));
